e61349d58972f5646a156455e2d101ebaaa0dc6d
Priyotama <chantyo.atmaja@gmail.com>
2023-06-19 04:35:27 +0700

    edit lagi

--- BelajarYok/View/Friends.xaml.cs
+++ BelajarYok/View/Friends.xaml.cs
@@ -12,9 +12,50 @@ namespace BelajarYok.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Friends : ContentPage
     {
+        private bool isAddImageButtonClicked = false;
+        private string searchText = string.Empty;
+
         public Friends()
         {
             InitializeComponent();
         }
+        private void OnSearchEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            searchText = e.NewTextValue;
+            UpdateContentVisibility();
+        }
+
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            isAddImageButtonClicked = true;
+            //addButton.BackgroundColor = Color.Transparent;
+            //addButton.TextColor = Color.FromHex("#bcfe2f");
+            //addButton.Text = "Added";
+            //friendText.Text = "Sudah di daftarkan sebagai teman";
+            UpdateContentVisibility();
+        }
+
+        private void OnAddImageButtonClicked(object sender, EventArgs e)
+        {
+            if (searchText == "admin")
+            {
+                isAddImageButtonClicked = true;
+
+                UpdateContentVisibility();
+            }
+        }
+
+        private void UpdateContentVisibility()
+        {
+            if (searchText == "admin" && isAddImageButtonClicked)
+            {
+                //contentFrame.IsVisible = true;
+            }
+            else
+            {
+                //contentFrame.IsVisible = false;
+            }
+        }
     }
 }
+    
